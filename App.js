a07c4c42c606372ebbe7dd30aa030ca7fd8444ee
animmore <zaskokazhenya@gmail.com>
2020-06-18 23:47:01 +0300

    rm trash imports

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React from "react";
-import { WelcomeScreen } from "./app/screens/WelcomeScreen";
-import { ViewImageScreen } from "./app/screens/ViewImageScreen";
-import { Card } from "./app/components/Card";
-import { ListItem } from "./app/components/ListItem";
-import { MessageScreen } from "./app/screens/MessageScreen";
-import { AppButton } from "./app/components/AppButton";
-import { AppTextInput } from "./app/components/AppTextInput";
-import { THEME } from "./app/theme";
-import { MyAccountScreen } from "./app/screens/MyAccountScreen";
-import { ListingsScreen } from "./app/screens/ListingsScreen";
-import { Icon } from "./app/components/Icon";
 import { Screen } from "./app/components/Screen";
+import { AppTextInput } from "./app/components/AppTextInput";
 
 export default function App() {
   return (
