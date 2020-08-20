e7346fb01b33751b24bdc02fc9e7bcdba7750dcb
animmore <zaskokazhenya@gmail.com>
2020-08-20 13:01:06 +0300

    permissions

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LoginScreen } from "./app/screens/LoginScreen";
 import { ListingEditScreen } from "./app/screens/ListingEditScreen";
+import { Screen } from "./app/components/Screen";
+import * as ImagePicker from "expo-image-picker";
+import { Button, Image } from "react-native";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library.");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const [imageUri, setImageUri] = useState();
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
