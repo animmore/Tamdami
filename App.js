7e7548cc45105150e702248eda0117a9036fa2e6
animmore <zaskokazhenya@gmail.com>
2020-08-21 14:24:47 +0300

    register screen & nav

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { LoginScreen } from './app/screens/LoginScreen';
-import { ListingEditScreen } from './app/screens/ListingEditScreen';
-import { Screen } from './app/components/Screen';
-import * as ImagePicker from 'expo-image-picker';
-import { Button, Image } from 'react-native';
-import { ImageInput } from './app/components/ImageInput';
-import { ImageInputList } from './app/components/ImageInputList';
-import { FormImagePicker } from './app/components/forms/FormImagePicker';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthNavigator } from './app/navigatiion/AuthNavigator';
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
-import { THEME } from "../theme";
-import { AppButton } from "../components/AppButton";
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { THEME } from '../theme';
+import { AppButton } from '../components/AppButton';
 
-export function WelcomeScreen() {
+export function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/background.jpg")}
+      source={require('../assets/background.jpg')}
       blurRadius={9}
     >
       <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Image style={styles.logo} source={require('../assets/logo-red.png')} />
         <Text style={styles.tagline}>Sell anything!</Text>
       </View>
 
       <View style={styles.buttonsContainer}>
-        <AppButton title={"login"} />
-        <AppButton title={"registration"} color={THEME.LIGHT_BLUE} />
+        <AppButton
+          title={'Login'}
+          onPress={() => navigation.navigate('Login')}
+        />
+        <AppButton
+          title={'Registration'}
+          color={THEME.LIGHT_BLUE}
+          onPress={() => navigation.navigate('Register')}
+        />
       </View>
     </ImageBackground>
   );
@@ -26,24 +33,24 @@ export function WelcomeScreen() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    width: "100%",
-    justifyContent: "flex-end",
-    alignItems: "center",
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   logoContainer: {
-    position: "absolute",
+    position: 'absolute',
     top: 70,
-    alignItems: "center",
+    alignItems: 'center',
   },
   logo: { width: 100, height: 100 },
   tagline: {
-    fontWeight: "600",
+    fontWeight: '600',
     fontSize: THEME.DEVICE_WINDOW_HEIGHT * 0.03,
     paddingVertical: 20,
   },
   buttonsContainer: {
     padding: 20,
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
   },
 });
